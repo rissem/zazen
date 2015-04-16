6aab62bb452c63386e3a6c83b0b9654b71580fcd
Mike Risse <rissem@gmail.com>
2015-04-15 20:35:37 -0700

    add jade to project, firePad integration

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var jade = require('jade');
+var fs = require('fs');
+var path = require('path');
 
 app.get("*", function(req, res){
   console.log(req.path);
-  res.send("hello there");
+  var editorTemplate = path.join(__dirname, "editor.jade");
+  var html = jade.renderFile(editorTemplate, {youAreUsingJade: true, filepath: req.path});
+
+  res.send(html);
 });
 
 app.post("*", function(req, res){
