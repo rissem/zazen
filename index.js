d07eb7974bf5e65b7239c7a07b545313c0a99a16
Mike Risse <rissem@gmail.com>
2015-04-21 14:20:13 -0700

    fully specify path

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var path = require('path');
 var bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/public', express.static('public'));
-app.use("/code-mirror", express.static('CodeMirror'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/code-mirror", express.static(path.join(__dirname, 'CodeMirror')));
 
 app.get("*", function(req, res, next){
   var editorTemplate = path.join(__dirname, "editor.jade");
